547da567a333a965669a2f993cbd4004a728e448
Vova Pesochniy <vpesochniy@yandex.com>
2025-05-26 09:14:13 +0300

    feat(nvim): Enhance navigation and text editing with new plugins
    Introduce nvim-surround, leap.nvim, precognition.nvim, and flit.nvim.
    Configure these plugins for advanced text objects and jump motions.
    Standardize keymap command syntax from <cmd> to <CMD> for consistency.
    Simplify Telescope find_files and live_grep keymaps to use defaults.
    Comment out 's' and 'S' keymaps to prevent conflicts with nvim-surround.
    Add new utility keymaps for redo (U), search/replace (?), and chmod +x.

--- neovim/.config/nvim/lua/config/keybinds.lua
+++ neovim/.config/nvim/lua/config/keybinds.lua
@@ -10,16 +10,16 @@ keymap("n", "N", "Nzzzv")
 keymap("n", "G", "Gzz")
 
 -- turn off highlighting for the previous search
-keymap('n', '<Esc>', '<Cmd>nohlsearch<Cr>')
-keymap('n', '<C-c>', '<Cmd>nohlsearch<Cr>')
+keymap('n', '<Esc>', '<CMD>nohlsearch<CR>')
+keymap('n', '<C-c>', '<CMD>nohlsearch<CR>')
 
 -- disable copying to the clipboard
 keymap({ 'n', 'v' }, 'd', '"_d')
 keymap({ 'n', 'v' }, 'D', '"_D')
 keymap({ 'n', 'v' }, 'c', '"_c')
 keymap({ 'n', 'v' }, 'C', '"_C')
-keymap({ 'n', 'v' }, 's', '"_s')
-keymap({ 'n', 'v' }, 'S', '"_S')
+-- keymap({ 'n', 'v' }, 's', '"_s')
+-- keymap({ 'n', 'v' }, 'S', '"_S')
 
 -- move focus to the window
 keymap('n', '<C-h>', '<C-w><C-h>')
@@ -39,34 +39,12 @@ keymap('t', '<Esc><Esc>', '<C-\\><C-n>')
 -- 'Ctrl + C' for exit insert mode
 keymap('i', '<C-c>', '<Esc>')
 
-keymap('n', '<leader>e', '<Cmd>Neotree toggle<CR>')
+keymap('n', '<leader>e', '<CMD>Neotree toggle<CR>')
 
-keymap('n', '<leader>ff', function()
-    require('telescope.builtin').find_files({
-        hidden = true,
-        no_ignore = true,
-        find_command = {
-            'fd', '--type', 'f', '--hidden', '--no-ignore', '.',
-            vim.fn.getcwd(),
-            vim.fn.expand("~/.config/nvim"),
-        },
-
-    })
-end)
-
-
-vim.keymap.set('n', '<leader>fg', function()
-    require('telescope.builtin').live_grep({
-        additional_args = { "--hidden", "--no-ignore" },
-        search_dirs = {
-            vim.fn.getcwd(),
-            vim.fn.expand("~/.config/nvim"),
-        },
-    })
-end, { desc = 'Live Grep (hidden and no-ignore)' })
-
-keymap('n', '<leader>fh', '<Cmd>Telescope help_tags<CR>')
-keymap('n', '<leader>fd', '<Cmd>Telescope diagnostics<CR>', { desc = '[F]ind [D]iagnostics' })
+keymap('n', '<leader>ff', '<CMD>Telescope find_files<CR>')
+keymap('n', '<leader>fg', '<CMD>Telescope live_grep<CR>')
+keymap('n', '<leader>fh', '<CMD>Telescope help_tags<CR>')
+keymap('n', '<leader>fd', '<CMD>Telescope diagnostics<CR>', { desc = '[F]ind [D]iagnostics' })
 
 -- This is where you enable features that only work
 -- if there is a language server active in the file
@@ -75,51 +53,51 @@ vim.api.nvim_create_autocmd('LspAttach', {
     callback = function(event)
         local opts = { buffer = event.buf }
 
-        keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
-        keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
-        keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
-        keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
-        keymap('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
-        keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
-        keymap('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
-        keymap('n', '<leader>r', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
-        keymap({ 'n', 'x' }, '<leader>l', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
-        keymap('n', '<leader>a', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+        keymap('n', 'K', '<CMD>lua vim.lsp.buf.hover()<CR>', opts)
+        keymap('n', 'gd', '<CMD>lua vim.lsp.buf.definition()<CR>', opts)
+        keymap('n', 'gD', '<CMD>lua vim.lsp.buf.declaration()<CR>', opts)
+        keymap('n', 'gi', '<CMD>lua vim.lsp.buf.implementation()<CR>', opts)
+        keymap('n', 'go', '<CMD>lua vim.lsp.buf.type_definition()<CR>', opts)
+        keymap('n', 'gr', '<CMD>lua vim.lsp.buf.references()<CR>', opts)
+        keymap('n', 'gs', '<CMD>lua vim.lsp.buf.signature_help()<CR>', opts)
+        keymap('n', '<leader>r', '<CMD>lua vim.lsp.buf.rename()<CR>', opts)
+        keymap({ 'n', 'x' }, '<leader>l', '<CMD>lua vim.lsp.buf.format({async = true})<CR>', opts)
+        keymap('n', '<leader>a', '<CMD>lua vim.lsp.buf.code_action()<CR>', opts)
     end,
 })
 
 -- Add a mark
-keymap("n", "M", "<cmd>lua require('harpoon.mark').add_file()<CR>")
+keymap("n", "M", "<CMD>lua require('harpoon.mark').add_file()<CR>")
 
 keymap("n", "<C-i>", "<C-i>")
 
 -- Toggle Harpoon UI
-keymap("n", "<Tab>", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<CR>")
-keymap("n", "<C-n>", "<cmd>lua require('harpoon.ui').nav_next()<CR>")
-keymap("n", "<C-p>", "<cmd>lua require('harpoon.ui').nav_prev()<CR>")
+keymap("n", "<Tab>", "<CMD>lua require('harpoon.ui').toggle_quick_menu()<CR>")
+keymap("n", "<C-n>", "<CMD>lua require('harpoon.ui').nav_next()<CR>")
+keymap("n", "<C-p>", "<CMD>lua require('harpoon.ui').nav_prev()<CR>")
 
 keymap("n", "<leader>u", vim.cmd.UndotreeToggle)
 keymap("n", "<leader>tt", vim.cmd.ToggleTerm)
 keymap("n", "<leader>tn", vim.cmd.TermNew)
 
-keymap('n', '<leader>co', "<Cmd>lua require'jdtls'.organize_imports()<CR>", { desc = 'Organize Imports' })
-keymap('n', '<leader>crv', "<Cmd>lua require('jdtls').extract_variable()<CR>", { desc = 'Extract Variable' })
-keymap('v', '<leader>crv', "<Esc><Cmd>lua require('jdtls').extract_variable(true)<CR>", { desc = 'Extract Variable' })
-keymap('n', '<leader>crc', "<Cmd>lua require('jdtls').extract_constant()<CR>", { desc = 'Extract Constant' })
-keymap('v', '<leader>crc', "<Esc><Cmd>lua require('jdtls').extract_constant(true)<CR>", { desc = 'Extract Constant' })
-keymap('v', '<leader>crm', "<Esc><Cmd>lua require('jdtls').extract_method(true)<CR>", { desc = 'Extract Method' })
-keymap("n", "<leader>jtc", "<Cmd>lua require'jdtls'.test_class()<CR>")
-keymap("n", "<leader>jtm", "<Cmd>lua require'jdtls'.test_nearest_method()<CR>")
-keymap('n', '<leader>jdr', "<Cmd>lua require'springboot-nvim'.boot_run()<CR>", { desc = "Spring Boot Run Project" })
-keymap('n', '<leader>jcc', "<Cmd>lua require'springboot-nvim'.generate_class()<CR>", { desc = "Java Create Class" })
-keymap('n', '<leader>jci', "<Cmd>lua require'springboot-nvim'.generate_interface()<CR>",
+keymap('n', '<leader>co', "<CMD>lua require'jdtls'.organize_imports()<CR>", { desc = 'Organize Imports' })
+keymap('n', '<leader>crv', "<CMD>lua require('jdtls').extract_variable()<CR>", { desc = 'Extract Variable' })
+keymap('v', '<leader>crv', "<Esc><CMD>lua require('jdtls').extract_variable(true)<CR>", { desc = 'Extract Variable' })
+keymap('n', '<leader>crc', "<CMD>lua require('jdtls').extract_constant()<CR>", { desc = 'Extract Constant' })
+keymap('v', '<leader>crc', "<Esc><CMD>lua require('jdtls').extract_constant(true)<CR>", { desc = 'Extract Constant' })
+keymap('v', '<leader>crm', "<Esc><CMD>lua require('jdtls').extract_method(true)<CR>", { desc = 'Extract Method' })
+keymap("n", "<leader>jtc", "<CMD>lua require'jdtls'.test_class()<CR>")
+keymap("n", "<leader>jtm", "<CMD>lua require'jdtls'.test_nearest_method()<CR>")
+keymap('n', '<leader>jdr', "<CMD>lua require'springboot-nvim'.boot_run()<CR>", { desc = "Spring Boot Run Project" })
+keymap('n', '<leader>jcc', "<CMD>lua require'springboot-nvim'.generate_class()<CR>", { desc = "Java Create Class" })
+keymap('n', '<leader>jci', "<CMD>lua require'springboot-nvim'.generate_interface()<CR>",
     { desc = "Java Create Interface" })
-keymap('n', '<leader>jce', "<Cmd>lua require'springboot-nvim'.generate_enum()<CR>", { desc = "Java Create Enum" })
-keymap('n', '<leader>jdc', "<Cmd>lua require'dap'.continue()<CR>")
-keymap('n', '<leader>jdb', "<Cmd>lua require'dap'.toggle_breakpoint()<CR>")
+keymap('n', '<leader>jce', "<CMD>lua require'springboot-nvim'.generate_enum()<CR>", { desc = "Java Create Enum" })
+keymap('n', '<leader>jdc', "<CMD>lua require'dap'.continue()<CR>")
+keymap('n', '<leader>jdb', "<CMD>lua require'dap'.toggle_breakpoint()<CR>")
 
 -- Easily split windows
-keymap("n", "<leader>v", ":vsplit<cr>", { desc = "[W]indow Split [V]ertical" })
+keymap("n", "<leader>v", ":vsplit<CR>", { desc = "[W]indow Split [V]ertical" })
 
 -- Stay in indent mode
 keymap("v", "<", "<gv", { desc = "Indent left in visual mode" })
@@ -157,12 +135,15 @@ function _lazygit_toggle()
     lazygit:toggle()
 end
 
-keymap("n", "<leader>g", "<cmd>lua _lazygit_toggle()<CR>", { noremap = true })
-keymap("n", "<leader>tf", "<cmd>lua _float_term_toggle()<CR>", { noremap = true })
+keymap("n", "<leader>g", "<CMD>lua _lazygit_toggle()<CR>", { noremap = true })
+keymap("n", "<leader>tf", "<CMD>lua _float_term_toggle()<CR>", { noremap = true })
 
-keymap("n", "<C-S-h>", "<Cmd>vertical resize +2<CR>", { silent = true })
-keymap("n", "<C-S-j>", "<Cmd>resize +2<CR>", { silent = true })
-keymap("n", "<C-S-k>", "<Cmd>resize -2<CR>", { silent = true })
-keymap("n", "<C-S-l>", "<Cmd>vertical resize -2<CR>", { silent = true })
+keymap("n", "<C-S-h>", "<CMD>vertical resize +2<CR>", { silent = true })
+keymap("n", "<C-S-j>", "<CMD>resize +2<CR>", { silent = true })
+keymap("n", "<C-S-k>", "<CMD>resize -2<CR>", { silent = true })
+keymap("n", "<C-S-l>", "<CMD>vertical resize -2<CR>", { silent = true })
 
 keymap("i", "<C-l>", "<Del>")
+keymap("n", "U", "<C-r>")
+keymap("n", "?", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+keymap("n", "<leader>x", "<CMD>!chmod +x %<CR>")
---
+++ neovim/.config/nvim/lua/plugins/flit.lua
@@ -0,0 +1,7 @@
+require('flit').setup {
+  keys = { f = 'f', F = 'F', t = 't', T = 'T' },
+  labeled_modes = "v",
+  clever_repeat = true,
+  multiline = true,
+  opts = {}
+}
